5fa9eec4ba1fc499f4c24fc478f86e56b533f3f5
kapil Narwani <124026841+kapil7071@users.noreply.github.com>
2026-02-03 00:55:06 +0530

    Update BillGeneratedApplication.java

--- src/main/java/com/bill/BillGenerated/BillGeneratedApplication.java
+++ src/main/java/com/bill/BillGenerated/BillGeneratedApplication.java
@@ -2,17 +2,18 @@ package com.bill.BillGenerated;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-	
 public class BillGeneratedApplication extends SpringBootServletInitializer {
 
-	@Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-    return builder.sources(BillGeneratedApplication.class);
-  }
-	public static void main(String[] args) {
-		SpringApplication.run(BillGeneratedApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BillGeneratedApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(BillGeneratedApplication.class, args);
+    }
 }
