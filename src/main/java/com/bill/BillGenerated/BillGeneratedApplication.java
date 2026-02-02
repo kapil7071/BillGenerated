bacf6eddc33b62916f319f1589980e22d35b4803
kapil Narwani <124026841+kapil7071@users.noreply.github.com>
2026-02-02 17:51:31 +0530

    Update BillGeneratedApplication.java

--- src/main/java/com/bill/BillGenerated/BillGeneratedApplication.java
+++ src/main/java/com/bill/BillGenerated/BillGeneratedApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-	@ComponentScan("com.bill")
+	
 public class BillGeneratedApplication {
 
 	public static void main(String[] args) {
